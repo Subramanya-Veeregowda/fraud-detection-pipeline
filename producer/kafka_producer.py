da3074c8c56052ec354f545be3a7787ec8295760
SubramanyaV <subramanyav2002@gmail.com>
2026-05-15 09:42:21 +0530

    setup spark cluster with kafka streaming pipeline

--- producer/kafka_producer.py
+++ producer/kafka_producer.py
@@ -8,7 +8,7 @@ import time
 fake = Faker()
 
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers='localhost:29092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8'),
     acks='all',
     retries=3
